622ec6669f53cf5d89a7ac48dde3003e39f01435
Gregory Nguenang <njethur@gmail.com>
2021-05-08 14:11:25 -0500

    Updated

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 // Define API routes here
 app.use(routes);
 
